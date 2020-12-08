8fbcab28e9a4fc43fb0d201c997106164c54023b
Kapish <kapish@Kapishs-MacBook-Air.local>
2020-12-08 23:23:48 +0530

    Code refactor for sum of two number with comma separated delimiter

--- src/main/java/com/example/calculator/service/CalculatorService.java
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -7,8 +7,21 @@ public class CalculatorService {
 
 	public int sum(String input) {
 		
-		if(input.isEmpty())
+		String[] numbers = input.split(",");
+		if(input.isEmpty()) {
 			return 0;
+		} 
+		
+		if(numbers.length == 1) {
+			Integer.parseInt(input); 
+		} else {
+			return getSum(numbers[0],numbers[1]);
+		}
+			
 		return Integer.parseInt(input);
+	}
+	
+	private int getSum(String num1, String num2) {
+		return Integer.parseInt(num1) + Integer.parseInt(num2);
 	}
 }
